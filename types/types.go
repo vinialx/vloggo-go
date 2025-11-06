cb6a559f851621ce66b1bb2a6c24186015647dec
viniciusaloise <vini.aloise.silva@gmail.com>
2025-11-05 21:40:42 -0300

    feat: add type definitions in types.go

--- types/types.go
+++ types/types.go
@@ -1,1 +1,48 @@
 package types
+
+type Paths struct {
+	Txt  string
+	Json string
+}
+
+type Count struct {
+	Txt  int
+	Json int
+}
+
+type SMTP struct {
+	Server   string   `env:"SMTP_HOST"`
+	Port     int      `env:"SMTP_PORT"`
+	Username string   `env:"SMTP_USERNAME"`
+	Password string   `env:"SMTP_PASSWORD"`
+	From     string   `env:"SMTP_FROM"`
+	To       []string `env:"SMTP_TO"`
+}
+
+type VLoggo struct {
+	Client    string
+	Json      bool
+	Debug     bool
+	Notify    bool
+	Console   bool
+	Throttle  int
+	Filecount Count
+	Directory Paths
+}
+
+type LogLevel string
+
+const (
+	Info  LogLevel = "INFO"
+	Warn  LogLevel = "WARN"
+	Error LogLevel = "ERROR"
+	Fatal LogLevel = "FATAL"
+	Debug LogLevel = "DEBUG"
+)
+
+type LogEntry struct {
+	Level   LogLevel `json:"level"`
+	Code    int      `json:"code"`
+	Caller  string   `json:"caller"`
+	Message string   `json:"message"`
+}
