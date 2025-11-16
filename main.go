d6446f736e7503c20f10a6b773465b58d550890f
viniciusaloise <vini.aloise.silva@gmail.com>
2025-11-15 23:58:47 -0300

    feat: add initial write functions in main.go

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package vloggo
 
 import (
 	"fmt"
+	"os"
 	"sync"
 
-	"github.com/vinialx/vloggo-go/config"
+	config "github.com/vinialx/vloggo-go/config"
+	services "github.com/vinialx/vloggo-go/internal"
 	types "github.com/vinialx/vloggo-go/types"
 )
 
 type VLoggo struct {
-	cfg types.VLoggoConfig
-	mu  sync.Mutex
+	mu sync.Mutex
+
+	cfg    types.VLoggoConfig
+	file   *services.FileService
+	format *services.FormatService
 }
 
 var (
@@ -43,7 +48,9 @@ func NewInstance(client string, opts ...config.Option) *VLoggo {
 	}
 
 	instance := &VLoggo{
-		cfg: cfg,
+		cfg:    cfg,
+		file:   services.NewFileService(cfg),
+		format: services.NewFormatService(cfg.Client),
 	}
 
 	instances[client] = instance
@@ -97,57 +104,57 @@ func ClearInstances() {
 }
 
 func Clone(base, client string, opts ...config.Option) *VLoggo {
-    mu.RLock()
-    baseInstance, exists := instances[base]
-    mu.RUnlock()
-    
-    if !exists {
-        fmt.Printf("[VLoggo] > [%s] [INFO] : instance %s not found\n",
-            config.Date(),
-            base,
-        )
-        return nil
-    }
-    
-    mu.RLock()
-    if existingInstance, exists := instances[client]; exists {
-        mu.RUnlock()
-        fmt.Printf("[VLoggo] > [%s] [INFO] : instance %s already exists\n",
-            config.Date(),
-            client,
-        )
-        return existingInstance
-    }
-    mu.RUnlock()
-    
-    cloneCfg := baseInstance.GetConfig()
-    cloneCfg.Client = client
-    
-    for _, opt := range opts {
-        if opt != nil {
-            opt(&cloneCfg)
-        }
-    }
-    
-    mu.Lock()
-    defer mu.Unlock()
-    
-    if existingInstance, exists := instances[client]; exists {
-        return existingInstance
-    }
-    
-    newInstance := &VLoggo{
-        cfg: cloneCfg,
-    }
-    instances[client] = newInstance
-    
-    fmt.Printf("[VLoggo] > [%s] [%s] [INFO] : instance cloned from %s\n",
-        client,
-        config.Date(),
-        base,
-    )
-    
-    return newInstance
+	mu.RLock()
+	baseInstance, exists := instances[base]
+	mu.RUnlock()
+
+	if !exists {
+		fmt.Printf("[VLoggo] > [%s] [INFO] : instance %s not found\n",
+			config.Date(),
+			base,
+		)
+		return nil
+	}
+
+	mu.RLock()
+	if existingInstance, exists := instances[client]; exists {
+		mu.RUnlock()
+		fmt.Printf("[VLoggo] > [%s] [INFO] : instance %s already exists\n",
+			config.Date(),
+			client,
+		)
+		return existingInstance
+	}
+	mu.RUnlock()
+
+	cloneCfg := baseInstance.GetConfig()
+	cloneCfg.Client = client
+
+	for _, opt := range opts {
+		if opt != nil {
+			opt(&cloneCfg)
+		}
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if existingInstance, exists := instances[client]; exists {
+		return existingInstance
+	}
+
+	newInstance := &VLoggo{
+		cfg: cloneCfg,
+	}
+	instances[client] = newInstance
+
+	fmt.Printf("[VLoggo] > [%s] [%s] [INFO] : instance cloned from %s\n",
+		client,
+		config.Date(),
+		base,
+	)
+
+	return newInstance
 }
 
 func (v *VLoggo) Update(opts ...config.Option) {
@@ -159,4 +166,59 @@ func (v *VLoggo) Update(opts ...config.Option) {
 			opt(&v.cfg)
 		}
 	}
+}
+
+func (v *VLoggo) log(level types.LogLevel, code, message string) {
+
+	entry := types.LogEntry{
+		Level:   level,
+		Code:    code,
+		Caller:  services.Caller(3),
+		Message: message,
+	}
+
+	line := v.format.Line(entry)
+
+	if v.cfg.Json {
+		jsonLine := v.format.JSONLine(entry)
+
+		if err := v.file.Write(line, jsonLine); err != nil {
+			fmt.Printf("[VLoggo] > [%s] [%s] [INFO] : failed to write to log file > %s\n",
+				v.cfg.Client,
+				v.format.Date(),
+				err,
+			)
+		}
+	} else {
+		if err := v.file.Write(line); err != nil {
+			fmt.Printf("[VLoggo] > [%s] [%s] [INFO] : failed to write to log file > %s\n",
+				v.cfg.Client,
+				v.format.Date(),
+				err,
+			)
+		}
+	}
+
+}
+
+func (v *VLoggo) Info(code, message string) {
+	v.log("INFO", code, message)
+}
+
+func (v *VLoggo) Warn(code, message string) {
+	v.log("WARN", code, message)
+}
+
+func (v *VLoggo) Debug(code, message string) {
+	v.log("DEBUG", code, message)
+}
+
+func (v *VLoggo) Error(code, message string) {
+	v.log("ERROR", code, message)
+}
+
+func (v *VLoggo) Fatal(code, message string) {
+	v.log("FATAL", code, message)
+
+	os.Exit(1)
 }
